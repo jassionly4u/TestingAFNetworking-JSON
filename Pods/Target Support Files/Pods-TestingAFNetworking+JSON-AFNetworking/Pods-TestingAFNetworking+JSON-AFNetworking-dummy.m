33c14ff360468d4c93b363b082e90252c7bdbd9a
Jasvinder Singh <singh.jasvinder84@gmail.com>
2015-04-01 11:24:52 +0530

    Initial Commit.

---
+++ Pods/Target Support Files/Pods-TestingAFNetworking+JSON-AFNetworking/Pods-TestingAFNetworking+JSON-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingAFNetworking_JSON_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_TestingAFNetworking_JSON_AFNetworking
+@end
--- TestingAFNetworking+JSON/JSViewController.m
+++ TestingAFNetworking+JSON/JSViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "JSViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
-@interface JSViewController ()
+@interface JSViewController ()<NSURLConnectionDelegate>
 
 @end
 
@@ -18,7 +19,55 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    AFHTTPRequestOperationManager* manager = [AFHTTPRequestOperationManager manager];
+    //AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializerWithWritingOptions:NSJSONWritingPrettyPrinted];
+
+    
+//    AFJSONRequestSerializer* requestSerializer = [AFJSONRequestSerializer serializer];
+//    [requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+//    [requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+//    manager.requestSerializer = requestSerializer;
+    
+    [manager.responseSerializer setAcceptableContentTypes:[NSSet setWithObjects:@"text/plain",@"application/json",@"text/html",nil]];
+    
+    NSDictionary *dateDic = @{@"data":@{@"name":@"name", @"age":@"40"}};
+    NSError *error;
+//    NSData *json = [NSJSONSerialization dataWithJSONObject:dateDic options:0 error:&error];
+//    NSString *s = [NSString stringWithUTF8String:[json bytes]];
+//    NSLog(@"error--->%@",error);
+//    NSLog(@"s--->%@",s);
+    
+    /*[manager POST:@"http://localhost/Testing/testJson.php"
+       parameters:dateDic
+          success:^(NSURLSessionDataTask *task, id responseObject) {
+              NSLog(@"response %@",responseObject);
+              
+              NSDictionary *dict = (NSDictionary *)responseObject;
+              NSLog(@"dict %@",[dict valueForKeyPath:@"data.age"]);
+              
+          }
+          failure:^(NSURLSessionDataTask *task, NSError *error) {
+              NSLog(@"eror %@",error);
+          }];*/
+    [manager POST:@"http://localhost/Testing/testJson.php" parameters:dateDic success:^(AFHTTPRequestOperation* operation, id responseObject) {
+        NSLog(@"response %@",responseObject);
+        
+    } failure:^(AFHTTPRequestOperation* operation, NSError* error) {
+        //ICityError icity = [[ICityError alloc] init]
+        NSLog(@"eror %@",error);
+        
+    }];
+                                   
 }
+
+-(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+{
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
